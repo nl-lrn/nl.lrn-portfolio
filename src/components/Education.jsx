f30dbbcdde7d6582c6550dab2284154e07543fd2
nl.lrn <noel.laurine.pro@outlook.fr>
2023-07-26 19:01:56 +0200

    ajouts skillsList

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,6 +1,7 @@
 import '../styles/Education.css';
 import { Chrono } from 'react-chrono';
 import { educationsList } from '../datas/educationsList';
+import { skillsList } from '../datas/skillsList';
 
 function Education() {
     return (
@@ -8,6 +9,15 @@ function Education() {
             <h2>Mes formations</h2>
             <div className="portfolio-timeline" style={{ width: "80%", height: "100%" }}>
                 <Chrono items={ educationsList } mode="VERTICAL_ALTERNATING"  theme={{primary: '#672112', secondary:"#f9d6fb", titleColor: '#672112', titleColorActive: '#672112', cardTitleColor: '#672112'}} hideControls='false'/>
+            </div>
+            <h2>Langages</h2>
+            <div className='portfolio-langage'>
+                {skillsList.map(skill => (
+                    <div key={skill.title} className='portfolio-education-skills'>
+                        <img src={skill.icon} alt={skill.title} className='portfolio-education-skills-img'/>
+                        <p>{skill.title}</p>
+                    </div>
+                ))}
             </div>
         </section>
     )
---
+++ src/datas/skillsList.js
@@ -0,0 +1,44 @@
+import html from "../assets/HTML.webp"
+import css from "../assets/CSS.webp"
+import javascript from "../assets/JS.webp"
+import seo from "../assets/SEO.webp"
+import react from "../assets/REACT.webp"
+import nodejs from "../assets/NODEJS.webp"
+import github from "../assets/GITHUB.webp"
+import notion from "../assets/notion.webp"
+
+
+export const skillsList = [
+    {
+        title: "HTML",
+        icon: html
+    },
+    {
+        title: "CSS",
+        icon: css
+    },
+    {
+        title: "JS",
+        icon: javascript
+    },
+    {
+        title: "SEO",
+        icon: seo
+    },
+    {
+        title: "REACT",
+        icon: react
+    },
+    {
+        title: "NODE JS",
+        icon: nodejs
+    },
+    {
+        title: "GitHub",
+        icon: github
+    },
+    {
+        title: "Notion",
+        icon: notion
+    },
+]
